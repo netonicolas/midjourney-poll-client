0f10b8507bc44e8f7de1357882e198424643006e
Nicolas <altelis@mbp-de-nico.home>
2022-09-01 19:19:03 +0200

    design error page

--- pages/error/isClose.js
+++ pages/error/isClose.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import styles from "../../styles/error.module.css";
 
 export default function isClose(){
   return (
-    <div>
-      <h1>Trop tard le sondage est fermé</h1>
-      <Link href={'/'} >Retour à l'accueil</Link>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Trop tard le sondage est fermé</h1>
+      <span class={styles.link}><Link  href={'/'} >Retour à l'accueil</Link></span>
     </div>
   );
 }
--- pages/error/notOpen.js
+++ pages/error/notOpen.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
-
+import styles from "../../styles/error.module.css";
 export default function notOpen(){
   return (
-    <div>
-      <h1>Trop tôt le sondage le sondage n'est pas encore ouvert</h1>
-      <Link href={'/'} >Retour à l'accueil</Link>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Trop tôt le sondage le sondage n'est pas encore ouvert</h1>
+      <span class={styles.link}><Link href={'/'} >Retour à l'accueil</Link></span>
     </div>
   );
 }
